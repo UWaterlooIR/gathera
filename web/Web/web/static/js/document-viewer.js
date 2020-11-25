800504ec60183b7f30805735644dc87a2d0de3ad
linhnhifm <43214352+linhnhifm@users.noreply.github.com>
2020-11-25 12:09:31 +0100

    Update document-viewer.js

--- web/Web/web/static/js/document-viewer.js
+++ web/Web/web/static/js/document-viewer.js
@@ -1067,7 +1067,7 @@ docView.prototype = {
       return "";
     }
 
-    function checkURL(str) {
+    function isURL(str) {
       var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
