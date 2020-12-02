f6a1197ffcab08c5d8d4daf280d48a70f9d31810
linhnhifm <43214352+linhnhifm@users.noreply.github.com>
2020-12-02 13:01:05 +0100

    Update document-viewer.js

--- web/Web/web/static/js/document-viewer.js
+++ web/Web/web/static/js/document-viewer.js
@@ -480,10 +480,11 @@ docView.prototype = {
 
     function updateMeta(content) {
       if (isURL(content) === true){
-        content = makeClickable(content)
+        content = content.link(content)
       }
       const elm = $(options.documentMetaSelector);
       elm.html(content);
+      elm.find('a').attr('target', '_blank');
     }
 
     function updateBody(content, styles) {
@@ -1164,26 +1165,6 @@ docView.prototype = {
         '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
       return !!pattern.test(str);
     }
-
-    function makeClickable(inputText) {
-        var replacedText, replacePattern1, replacePattern2, replacePattern3;
-    
-        //URLs starting with http://, https://, or ftp://
-        replacePattern1 = /(\b(https?|ftp):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
-        replacedText = inputText.replace(replacePattern1, '<a href="$1" target="_blank">$1</a>');
-    
-        //URLs starting with "www." (without // before it, or it'd re-link the ones done above).
-        replacePattern2 = /(^|[^\/])(www\.[\S]+(\b|$))/gim;
-        replacedText = replacedText.replace(replacePattern2, '$1<a href="http://$2" target="_blank">$2</a>');
-    
-        //Change email addresses to mailto:: links.
-        replacePattern3 = /(([a-zA-Z0-9\-\_\.])+@[a-zA-Z\_]+?(\.[a-zA-Z]{2,6})+)/gim;
-        replacedText = replacedText.replace(replacePattern3, '<a href="mailto:$1">$1</a>');
-    
-        return replacedText;
-    }
-		
-
 
     return this._init();
   },
